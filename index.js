460a36c19b6d8a8b6a25e908db9520f0d7ab8a63
Juan Camilo <jcflorezpaez@gmail.com>
2024-09-18 20:07:30 -0500

    Instalación de Mongoose para coneccion a BD + Configuración general de index.js (Express)

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+import connectDB from './Database/config';
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+const corsOptions = {
+    origin: process.env.FRONTEND_URI || '*',
+    optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+app.use(express.static('public'));
+app.use(express.json());
+
+connectDB();
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
