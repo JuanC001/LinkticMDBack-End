55f14597286188d30942b3ae4abf0f2df4ab8462
Juan <jcflorezpaez@gmail.com>
2024-09-19 11:27:03 -0500

    Modificación en AuthController para recibir el inicio de sesión y tambien se añadío la generación de un JWT

--- Controllers/Auth.controller.js
+++ Controllers/Auth.controller.js
@@ -2,11 +2,48 @@ import { request, response } from "express";
 import bcrypt from "bcryptjs";
 import User from "../Models/User.js";
 
+import { generateJWT } from "../Helpers/jwt.js";
 
 const authController = {}
 
-authController.login = (req = request, res = response) => {
+authController.login = async (req = request, res = response) => {
+    const { email, password } = req.body;
+    try {
+        let usuario = await User.findOne({ email })
+        if (!usuario) {
+            res.send("Usuario no encontrado").status(404);
+            return
+        }
 
+        const validPassword = bcrypt.compareSync(password, usuario.password);
+        if (!validPassword) {
+            res.send("Contraseña incorrecta").status(400);
+            return
+        }
+
+        const token = await generateJWT(usuario._id, usuario.name);
+        if(!token){
+            res.send("Error al generar el token").status(500);
+            return
+        }
+        res.send({ 
+            uid: usuario.id,
+            name: usuario.name,
+            lastname: usuario.lastname,
+            username: usuario.username,
+            email: usuario.email,
+            reservations: usuario.reservations,
+            token
+         }).status(200);
+
+
+
+    } catch (error) {
+        console.error(error);
+        res.send("Hubo un error").status(500);
+        return
+
+    }
 }
 
 authController.register = async (req = request, res = response) => {
